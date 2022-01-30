0f3c669d1129d4e4c99613ca79c49b8a01c3ae37
goreleaserbot <goreleaser@carlosbecker.com>
2022-01-30 16:10:44 +0900

    Brew formula update for dimg version v0.1.0

--- Formula/dimg.rb
+++ Formula/dimg.rb
@@ -5,32 +5,43 @@
 class Dimg < Formula
   desc "TUI for docker pull"
   homepage "https://github.com/kohbis/dimg"
-  version "0.0.6"
-  bottle :unneeded
+  version "0.1.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/kohbis/dimg/releases/download/v0.0.6/dimg_0.0.6_Darwin_x86_64.tar.gz"
-      sha256 "50c11b17ebe385a2aed4aa318207cdce91c2ddf5b3df5c7d796dcc28713d3a88"
+      url "https://github.com/kohbis/dimg/releases/download/v0.1.0/dimg_0.1.0_Darwin_x86_64.tar.gz"
+      sha256 "6be358de425f05b797d07c56c8d905f1c87149c69cf4fe814be5306e7b0a3996"
+
+      def install
+        bin.install "dimg"
+      end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/kohbis/dimg/releases/download/v0.0.6/dimg_0.0.6_Darwin_arm64.tar.gz"
-      sha256 "4dddb7d5240207f85da7380f85759b657797e8013004646007ce11decb7ca781"
+      url "https://github.com/kohbis/dimg/releases/download/v0.1.0/dimg_0.1.0_Darwin_arm64.tar.gz"
+      sha256 "94879bd887f7ec05156fe2aa4ebf942d44115ab716b74ed71297e3f8828cdc9d"
+
+      def install
+        bin.install "dimg"
+      end
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/kohbis/dimg/releases/download/v0.0.6/dimg_0.0.6_Linux_x86_64.tar.gz"
-      sha256 "5e68b79d69b8acb62810e20ddf028f7bf2ecfda87519a0eb87c0546b1641a08e"
-    end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kohbis/dimg/releases/download/v0.0.6/dimg_0.0.6_Linux_arm64.tar.gz"
-      sha256 "78f0ac270e3aaa3a76e697bc5ed88056df3d7b1f38fe5144c7b0dd46cf8971a6"
-    end
-  end
+      url "https://github.com/kohbis/dimg/releases/download/v0.1.0/dimg_0.1.0_Linux_arm64.tar.gz"
+      sha256 "999509ceb1351788842119847499de70c5132cdadaf23b72d0a7c38a4d14d29d"
 
-  def install
-    bin.install "dimg"
+      def install
+        bin.install "dimg"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/kohbis/dimg/releases/download/v0.1.0/dimg_0.1.0_Linux_x86_64.tar.gz"
+      sha256 "7c41f7df56d6e27ec81478bbc2f644fef2afb4f01997aa1c6fa5663083960e60"
+
+      def install
+        bin.install "dimg"
+      end
+    end
   end
 end
